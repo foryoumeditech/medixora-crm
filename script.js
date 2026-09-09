b0527c675a49fe0d67b30aed3ca1acf8fc92df74
foryoumeditech <97326621+foryoumeditech@users.noreply.github.com>
2026-09-09 22:40:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ leadForm.addEventListener('submit', (e) => {
 // Save to LocalStorage and Update UI
 function saveAndRefresh() {
  localStorage.setItem(STORAGE_KEY, JSON.stringify(leads));
- fetch('https://script.google.com/macros/s/AKfycbys3D4vOZCj25Z6mEvDU51oRrLQ8smSw7jiuh6Ibm0hvupWh3eOTCIxnJhkHguiJNAa/exec', {
+ fetch('https://script.google.com/macros/s/AKfycbxfUIQ-DIQZHsPNopX4WPXODlE1Oa9kwE9yhVvBSgGhUcqKPw-aR7ijo5yjbh-55LuW/exec', {
  method: 'POST',
  mode: 'no-cors',
  headers: {
